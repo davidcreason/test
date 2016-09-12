e0302c504c280f99b6fd1a78569ded207e9a54cf
David Creason <david.creason@tealium.com>
2016-09-12 15:58:24 -0400

    a
    b

--- copy_json.js
+++ copy_json.js
@@ -1,15 +1,15 @@
 {
 	"id" : "iq_selective_copy_2",
-	"icon_url" : "http://davidcreason.com/tealium/js/export_tealium_tools.png",
+	"icon_url" : "https://solutions.tealium.net/hosted/tealiumTools/export_tealium_tools/export_tealium_tools.png",
 	"description" : "This Tealium Tool  allows you to select individual items from each tab in IQ, copy just those (vs. Copy Profile) and then Paste that in a new profile",
 	"title" : "Copy/Paste Test",
 	"scripts" : {
 		"copy_tt" : {
 			"title" : "Copy/Paste Test",
 			"remote_template" : true,
-			"template" : "http://davidcreason.com/tealium/js/tealium_iq_selective_copy2.html",
+			"template" : "https://raw.githubusercontent.com/davidcreason/test/master/tealium_iq_selective_copy2.html",
 			"remote_js" : true,
-			"js" : "http://davidcreason.com/tealium/js/tealium_iq_selective_copy2.js",
+			"js" : "https://rawgit.com/davidcreason/test/master/tealium_iq_selective_copy2.js",
 			"auto_inject" : true
 		}
 	}
